4271b96cc71dc772f3c1fad3c4da5f9103e45cc1
David Danko <dcdanko@gmail.com>
2018-05-20 00:26:13 -0400

    database install

--- databases.py
+++ databases.py
@@ -14,6 +14,7 @@ def get_downloader(dirname, uri):
         fname = uri.split('/')[-1]
         if isfile(fname):
             return
+        print(f'Downloading {fname}')
         urlretrieve(uri, filename=fname)
         if ('.tar.gz' in fname) or ('.tgz' in fname):
             tar = tarfile.open(fname)
